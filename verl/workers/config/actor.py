06655663c4fe4a17b717bb95ed348cc2d661b64e
杨森 <y775392901@icloud.com>
2025-12-01 14:05:13 +0800

    [feat] (actor) add sft_coeff config

--- verl/workers/config/actor.py
+++ verl/workers/config/actor.py
@@ -110,6 +110,7 @@ class ActorConfig(BaseConfig):
     use_torch_compile: bool = True
     kl_loss_coef: float = 0.001
     kl_loss_type: str = "low_var_kl"
+    sft_coeff: float = 0
     ppo_epochs: int = 1
     shuffle: bool = False
     checkpoint: CheckpointConfig = field(default_factory=CheckpointConfig)
