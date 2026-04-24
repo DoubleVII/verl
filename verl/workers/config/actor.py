41efea90253cb24db7085aa00cbbcd7be2c03f36
double7 <y775392901@icloud.com>
2026-04-24 21:36:58 +0800

    [feat](config): add sft_coef arg config

--- verl/workers/config/actor.py
+++ verl/workers/config/actor.py
@@ -160,6 +160,7 @@ class ActorConfig(BaseConfig):
     use_torch_compile: bool = True
     kl_loss_coef: float = 0.001
     kl_loss_type: str = "low_var_kl"
+    sft_coef: float = 0
     ppo_epochs: int = 1
     shuffle: bool = False
     data_loader_seed: int = 1
